30762d3552c4782f8bb6c8a36fc7beac34e531b9
numtel <ben@latenightsketches.com>
2022-06-15 16:26:42 -0700

    Remove homepage hyphen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,10 +13,10 @@ export default class Home extends Template {
         <h1>Democratic Capital</h1>
       </div>
       <div class="blue window">
-        <p>Welcome to Democratic Capital, the place where your dreams of exercising less-than absolute power come true!</p>
+        <p>Welcome to Democratic Capital, the place where your dreams of exercising less than absolute power come true!</p>
       <a class="button" href="/${config.contracts.VerifiedGroupFactory.address}" $${this.link}>Browse Groups...</a>
       </div>
-      <div class="blue window">
+      <div class="white window">
         <h3>Running on Optimism</h3>
         <p>Democratic Capital is deployed on the <a href="https://optimism.io">Optimism Ethereum Layer 2 Network</a> in order to obtain fast transactions and low gas fees.</p>
         <h3>Verified Humans</h3>
