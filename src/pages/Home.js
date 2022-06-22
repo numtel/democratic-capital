3a4f81c8b0ead89b07684ca8780acd8e12b9dec1
numtel <ben@latenightsketches.com>
2022-06-21 17:31:23 -0700

    Popup homepage links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,9 +18,9 @@ export default class Home extends Template {
       </div>
       <div class="white window">
         <h3>Running on Optimism</h3>
-        <p>Democratic Capital is deployed on the <a href="https://optimism.io">Optimism Ethereum Layer 2 Network</a> in order to obtain fast transactions and low gas fees.</p>
+        <p>Democratic Capital is deployed on the <a $${this.link} href="https://optimism.io">Optimism Ethereum Layer 2 Network</a> in order to obtain fast transactions and low gas fees.</p>
         <h3>Verified Humans</h3>
-        <p>Every user of Democratic Capital must verify as a unique human  on <a href="https://coinpassport.net/">Coinpassport</a>. This ensures that votes are fair and not determined solely by who has the biggest wallet.</p>
+        <p>Every user of Democratic Capital must verify as a unique human  on <a $${this.link} href="https://coinpassport.net/">Coinpassport</a>. This ensures that votes are fair and not determined solely by who has the biggest wallet.</p>
         <h3>Unlimited Groups</h3>
         <p>Create a group for your friends, or for the next great social movement. Either way, you'll have the ability to decide collectively which contracts with which to transact.</p>
       </div>
